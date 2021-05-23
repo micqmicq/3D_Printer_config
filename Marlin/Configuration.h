7f0a90f3b6c6b056344b0fb419dcfc0953d61c82
Svviety <micorex13@gmail.com>
2021-05-23 04:22:09 +0200

    adjust DEFAULT_AXIS_STEPS_PER_UNIT

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -741,7 +741,7 @@
  * Override with M92
  *                                      X, Y, Z, E0 [, E1[, E2...]]
  */
-#define DEFAULT_AXIS_STEPS_PER_UNIT   { 850, 850, 850, 550 } //{ 80, 80, 400, 380 }
+#define DEFAULT_AXIS_STEPS_PER_UNIT   { 800, 800, 800, 550 } //{ 80, 80, 400, 380 }
 
 
 /**
@@ -1128,8 +1128,8 @@
 #define Y_BED_SIZE 220
 
 // Travel limits (mm) after homing, corresponding to endstop positions.
-#define X_MIN_POS 0
-#define Y_MIN_POS 0
+#define X_MIN_POS -10
+#define Y_MIN_POS -30
 #define Z_MIN_POS 0
 #define X_MAX_POS X_BED_SIZE
 #define Y_MAX_POS Y_BED_SIZE
