bf716e1e82178db634a0665f6cb31cf39adf0fa9
Svviety <micorex13@gmail.com>
2021-08-23 18:40:21 +0200

    readjusted Z_SERVO_ANGLES after repleacing prebe's servo

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -896,7 +896,7 @@
  * Z Servo Probe, such as an endstop switch on a rotating arm.
  */
 #define Z_PROBE_SERVO_NR 0       // Defaults to SERVO 0 connector.
-#define Z_SERVO_ANGLES { 170, 20 } // Z Servo Deploy and Stow angles
+#define Z_SERVO_ANGLES { 150, 0 } // Z Servo Deploy and Stow angles
 
 /**
  * The BLTouch probe uses a Hall effect sensor and emulates a servo.
